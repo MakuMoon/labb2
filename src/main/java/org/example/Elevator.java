23f788684e0f05b293be4e880b51c8bd91c7256b
Gaurgle <justwannagroove@gmail.com>
2024-12-05 14:04:02 +0100

    Elevator done

---
+++ src/main/java/org/example/Elevator.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import javax.swing.plaf.metal.MetalIconFactory;
+import java.util.Scanner;
+
+public class Elevator {
+    public int currentFloor;
+    public int TOP_FLOOR;
+    public int BOTTOM_FLOOR;
+    boolean doorsClosed;
+    public boolean inTransit;
+
+    public Elevator (int currentFloor, int topFloor, int bottomFloor, boolean doorsClosed, boolean inTransit) {
+        this.currentFloor = currentFloor;
+        this.TOP_FLOOR = topFloor;
+        this.BOTTOM_FLOOR = bottomFloor;
+        this.doorsClosed = doorsClosed;
+        this.inTransit = inTransit;
+    }
+
+    public void moveToFloor(int floor) throws InterruptedException {
+        openDoors();
+        System.out.print("Doors open, enter elevator.");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(".");
+            Thread.sleep(500);
+        }
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter floor nr: \n");
+        floor = scanner.nextInt();
+
+        closeDoors();
+
+        this.currentFloor = floor;
+        inTransit = true;
+        System.out.print("Travelling");
+         for (int i = 0; i < floor; i++) {
+             System.out.print(".");
+             Thread.sleep(500);
+
+         }
+
+         System.out.println("\nArrived");
+         openDoors();
+
+
+    }
+
+    public void openDoors() {
+        doorsClosed = false;
+    }
+
+    public void closeDoors() {
+        doorsClosed = true;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Elevator elevator = new Elevator(5, 5, 5, true, true);
+        elevator.moveToFloor(4);
+    }
+}
