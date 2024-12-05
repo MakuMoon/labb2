11324ee64b9df09093057e1dad7c5b91d86321d7
MakuMoon <78385721+MakuMoon@users.noreply.github.com>
2024-12-05 14:06:53 +0100

    deck

--- src/main/java/org/example/cards/Card.java
+++ src/main/java/org/example/cards/Card.java
@@ -5,6 +5,13 @@ public class Card {
     private String suit;
     private String rank;
 
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+
     @Override
     public String toString() {
         return rank + " of " + suit;
